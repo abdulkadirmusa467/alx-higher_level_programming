def61975498de54bf41cb3025fcf9f508bfc2aff
AbdulkadirMusa <abdulkadirmusa467@gmail.com>
2022-07-04 16:28:20 +0000

    Tasks

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -15,4 +15,4 @@ def inherits_from(obj, a_class):
     Returns: True or False
     """
 
-    return isinstance(obj, a_class) and type(obj) !=
+    return isinstance(obj, a_class) and type(obj) != a_class
