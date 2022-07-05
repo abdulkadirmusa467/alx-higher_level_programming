50e50a5688b6b1967a0cc347aa17adb61e33899c
AbdulkadirMusa <abdulkadirmusa467@gmail.com>
2022-07-05 12:55:53 +0000

    Tasks

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -10,6 +10,7 @@ prints the following statistics:
 
 def print_stats(size, status_codes):
     """Print accumulated metrics.
+
     Args:
         size (int): The accumulated read file size.
         status_codes (dict): The accumulated count of status codes.
@@ -21,10 +22,12 @@ def print_stats(size, status_codes):
 
 if __name__ == "__main__":
     import sys
+
     size = 0
     status_codes = {}
     valid_codes = ['200', '301', '400', '401', '403', '404', '405', '500']
     count = 0
+
     try:
         for line in sys.stdin:
             if count == 10:
@@ -33,10 +36,12 @@ if __name__ == "__main__":
             else:
                 count += 1
             line = line.split()
+
             try:
                 size += int(line[-1])
             except (IndexError, ValueError):
                 pass
+
             try:
                 if line[-2] in valid_codes:
                     if status_codes.get(line[-2], -1) == -1:
@@ -45,7 +50,9 @@ if __name__ == "__main__":
                         status_codes[line[-2]] += 1
             except IndexError:
                 pass
+
         print_stats(size, status_codes)
+
     except KeyboardInterrupt:
         print_stats(size, status_codes)
         raise
